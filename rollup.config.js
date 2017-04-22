fa460b409be1df02dfaafd69a08353317c68fc03
Zarni Phyo <zarniphyo95@gmail.com>
2017-04-22 21:26:32 +0630

    chore(lint): Add eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel'
+import eslint from 'rollup-plugin-eslint'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
@@ -7,23 +8,22 @@ import { minify } from 'uglify-js-harmony'
 
 export default {
   entry: 'src/index.js',
-  // moduleName: 'format',
   targets: [
     { dest: 'dist/bundle.cjs.js', format: 'cjs' },
-    // { dest: 'dist/bundle.umd.js', format: 'umd' },
     { dest: 'dist/bundle.es.js', format: 'es' },
   ],
   sourceMap: true,
   plugins: [
-    babel({
-      exclude: 'node_modules/**',
-    }),
     resolve({
       jsnext: true,
       main: true,
       browser: true,
     }),
     commonjs(),
+    eslint(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
     replace({
       exclude: 'node_modules/**',
       ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
